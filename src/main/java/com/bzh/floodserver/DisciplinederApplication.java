ffe28e2ff6782efaf6a75e0db19dbb3e7a91c18c
bzh <2422148686@qq.com>
2018-12-26 15:16:23 +0800

    test1

--- src/main/java/com/bzh/floodserver/DisciplinederApplication.java
+++ src/main/java/com/bzh/floodserver/DisciplinederApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@MapperScan("com.bzh.floodserver.mapper")
+@MapperScan("com.bzh.floodserver.mapper") //扫描mapper
 @EnableTransactionManagement //开启事务
 @SpringBootApplication
 public class DisciplinederApplication {
